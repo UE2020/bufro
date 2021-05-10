2ca8999f982ab703de8eabb29d80147560afff89
BlueCannonBall <35404206+BlueCannonBall@users.noreply.github.com>
2021-05-10 11:58:50 -0400

    more changes

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 ffi:
 		cargo build --release
-		cbindgen --config cbindgen.toml --crate bufro --output bufro.h
-		cd c_examples && g++ main.cpp -g -lGL -lGLU -lX11 -lXrandr -lglfw -L ../target/release -lbufro
+		cbindgen --config cbindgen.toml --crate bufro --output include/bufro.h
+
 install:
-		mv target/release/libbufro.so /usr/lib
+		cp target/release/libbufro.so /usr/lib/libbufro.so
+		cp include/* /usr/include
---
+++ include/bufro.h
@@ -0,0 +1,47 @@
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+
+/// A basic window-less renderer (though you can always just load the function pointers of a window)
+struct Renderer;
+
+/// Represents a color with values from 0-1
+struct Color {
+    float r;
+    float g;
+    float b;
+    float a;
+};
+
+
+extern "C" {
+
+void bfr_circle(Renderer *renderer, float x, float y, float r, Color color);
+
+Color bfr_color8(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+Color bfr_colorf(float r, float g, float b, float a);
+
+Renderer *bfr_create_surface(const void *(*loader)(const char*));
+
+void bfr_destroy(Renderer *renderer);
+
+void bfr_flush(Renderer *renderer);
+
+void bfr_rect(Renderer *renderer,
+              float x,
+              float y,
+              float width,
+              float height,
+              float angle,
+              Color color);
+
+void bfr_resize(Renderer *renderer, int32_t width, int32_t height);
+
+void bfr_set_clear_color(Renderer *renderer, Color color);
+
+} // extern "C"
